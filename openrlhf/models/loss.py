Fixing exploding gradient issue

openrlhf/models/loss.py
@@ -5,7 +5,7 @@ import torch.distributed as dist
 import torch.nn as nn
 import torch.nn.functional as F
 
-from .utils import masked_mean
+from .utils import agg_loss, masked_mean
 
 
 class GPTLMLoss(nn.Module):
@@ -87,11 +87,13 @@ class PolicyLoss(nn.Module):
         enable_vllm_is_correction: bool = False,
         vllm_is_truncated_threshold: list = None,
         vllm_is_correction_type: str = "tis",
+        loss_agg_mode: str = None,
     ) -> None:
         super().__init__()
         self.clip_eps_low = clip_eps_low
         self.clip_eps_high = clip_eps_high
         self.token_level_loss = token_level_loss
+        self.loss_agg_mode = loss_agg_mode
         self.dual_clip = dual_clip
         self.policy_loss_type = policy_loss_type
         self.enable_vllm_is_correction = enable_vllm_is_correction
@@ -172,11 +174,13 @@ class PolicyLoss(nn.Module):
                 loss = vllm_is * loss
             vllm_kl = masked_mean(rollout_log_probs - old_log_probs, action_mask, dim=None)
 
-        loss = (
-            masked_mean(loss, action_mask, dim=None)
-            if self.token_level_loss
-            else masked_mean(loss, action_mask, dim=-1).mean()
-        )
+        if self.loss_agg_mode is not None:
+            loss = agg_loss(loss, action_mask, mode=self.loss_agg_mode)
+        elif self.token_level_loss:
+            loss = masked_mean(loss, action_mask, dim=None)
+        else:
+            loss = masked_mean(loss, action_mask, dim=-1).mean()
+        # Metrics always use token-mean (independent of loss aggregation)
         clip_ratio = masked_mean(torch.lt(surr2, surr1).float(), action_mask, dim=None)
         ppo_kl = masked_mean(-log_ratio.detach(), action_mask, dim=None)
         return loss, clip_ratio, ppo_kl, vllm_kl

openrlhf/trainer/ray/ppo_actor.py
@@ -14,7 +14,7 @@ from tqdm import tqdm
 from transformers.trainer import get_scheduler
 
 from openrlhf.models import Actor, PolicyLoss
-from openrlhf.models.utils import compute_approx_kl, masked_mean
+from openrlhf.models.utils import agg_loss, compute_approx_kl, masked_mean
 from openrlhf.trainer.ppo_utils.experience_maker import Experience
 from openrlhf.utils import get_tokenizer
 from openrlhf.utils.deepspeed import DeepspeedStrategy
@@ -78,6 +78,7 @@ class ActorPPOTrainer(ABC):
                 self.args.vllm_is_truncated_threshold if self.args.enable_vllm_is_correction else None
             ),
             vllm_is_correction_type=self.args.vllm_is_correction_type,
+            loss_agg_mode=getattr(self.args, "loss_agg_mode", None),
         )
 
         # Mixtral 8x7b
@@ -285,7 +286,11 @@ class ActorPPOTrainer(ABC):
             else:
                 kl = torch.zeros_like(action_log_probs)
                 logprobs_diff = torch.zeros_like(action_log_probs)
-            kl_loss = masked_mean(kl, experience.action_mask)
+            loss_agg_mode = getattr(self.args, "loss_agg_mode", None)
+            if loss_agg_mode is not None:
+                kl_loss = agg_loss(kl, experience.action_mask, mode=loss_agg_mode)
+            else:
+                kl_loss = masked_mean(kl, experience.action_mask)
             logprobs_diff = masked_mean(logprobs_diff, experience.action_mask)
             experience.info["kl"] = kl_loss.detach()
             experience.info["logprobs_diff"] = logprobs_diff.detach()
@@ -298,7 +303,15 @@ class ActorPPOTrainer(ABC):
             loss += output.aux_loss * self.args.aux_loss_coef
         # entropy loss
         if self.args.entropy_loss_coef is not None:
-            entropy_loss = masked_mean(output.entropy[:, -experience.action_mask.shape[1] :], experience.action_mask)
+            loss_agg_mode = getattr(self.args, "loss_agg_mode", None)
+            if loss_agg_mode is not None:
+                entropy_loss = agg_loss(
+                    output.entropy[:, -experience.action_mask.shape[1] :],
+                    experience.action_mask,
+                    mode=loss_agg_mode,
+                )
+            else:
+                entropy_loss = masked_mean(output.entropy[:, -experience.action_mask.shape[1] :], experience.action_mask)
             if self.args.entropy_loss_coef != 0:
                 loss -= entropy_loss * self.args.entropy_loss_coef
 